ef5323f3cf49fe8b55cd08ad9e82a93d6fc67734
kevin-it237 <ngaleuabel@gmail.com>
2021-01-27 10:27:40 +0100

    Make api for saving responses

--- api/routes/spResponseRoutes.js
+++ api/routes/spResponseRoutes.js
@@ -9,12 +9,12 @@ router.post("/", (req, res, next) => {
             message: "YDMS_Org_id field not found!"
         });
     }
-    if(req.body.YDMS_SP_id) {
+    if(!req.body.YDMS_SP_id) {
         return res.status(401).send({
             message: "YDMS_SP_id field not found!"
         });
     }
-    if(req.body.response) {
+    if(!req.body.response) {
         return res.status(401).send({
             message: "response field not found!"
         });
@@ -27,14 +27,30 @@ router.post("/", (req, res, next) => {
         weight_response: req.body.weight ? req.body.weight : 0
     }
 
-    SPResponse.create(response)
-    .then(response => {
-        res.status(200).send({ 
-            ...response,
-            message: "Response saved successfully!" });
-    })
-    .catch(err => {
-        res.status(500).send({ message: err.message });
+    // Check if question is already anwsered
+    SPResponse.findOne({
+        where: {
+            organisationYDMSOrgId: req.body.YDMS_Org_id,
+            surveyProtocolYDMSSPId: req.body.YDMS_SP_id
+        }
+    }).then(resp => {
+        if (resp) {
+            res.status(400).send({
+                message: "This organisation has already anwsered this question!"
+            });
+            return;
+        } else {
+            // Save the response
+            SPResponse.create(response)
+            .then(response => {
+                res.status(200).send({ 
+                    data: response,
+                    message: "Response saved successfully!" });
+            })
+            .catch(err => {
+                res.status(500).send({ message: err.message });
+            });
+        }
     });
 });
 
