d61efadc1c3eaa2d19dd749fb42ec450c0c45271
kevin-it237 <ngaleuabel@gmail.com>
2021-02-16 07:33:29 +0100

    Query summary for EA added

--- api/routes/kpisRoutes.js
+++ api/routes/kpisRoutes.js
@@ -77,6 +77,31 @@ router.get("/afcac/:kpiId", (req, res, next) => {
     });
 });
 
+// KPIs data summary
+router.get("/summary/:orgType", (req, res, next) => {
+    const orgType = req.params.orgType
+
+    const queryPromise = 
+    db.sequelize.query(
+        'SELECT ydms_kpis.YDMS_KPIs_id, ydms_kpis.KPIs_label, SUM(survey_protocols.weight) as totalweight, SUM(sp_responses.weight_response) as custom_weight, SUM(sp_responses.questionnaire_response) as response, SUM((CASE WHEN sp_responses.questionnaire_response > 0 THEN survey_protocols.weight ELSE 0 END)) as weight, COUNT(*) as totalSP FROM `survey_protocols`, `ydms_kpis`, `sp_responses` WHERE survey_protocols.YDMSKPIYDMSKPIsId=ydms_kpis.YDMS_KPIs_id AND survey_protocols.YDMS_SP_Id=sp_responses.surveyProtocolYDMSSPId AND ydms_kpis.KPIs_org_type=? GROUP BY survey_protocols.YDMSKPIYDMSKPIsId',
+        {
+          replacements: [orgType],
+          type: QueryTypes.SELECT
+        }
+    );
+
+    queryPromise
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving datas"
+        });
+    });
+});
+
 // KPIs listing
 router.get("/org/:type", (req, res, next) => {
     const type = req.params.type
